b42ddbce9483a82c2ae7a10cddef46827101894b
kevinRodriguezCastro <10208@cifpceuta.es>
2024-01-09 19:31:27 +0100

    Initial commit

---
+++ app/src/main/java/com/cifpceuta/appplanificafirebase/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cifpceuta.appplanificafirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
